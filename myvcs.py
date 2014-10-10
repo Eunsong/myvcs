02ad15245bb32c6f3f8d171158fbadc60b9fea91
Eunsong <eunsong.choi@gmail.com>
2014-10-10 14:47:44 -0400

    add checkout particular snapshot

--- myvcs.py
+++ myvcs.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import os
+import argparse
 
 def backup():
 	""" a command line tool that recursively copies everything in the current directory to a directory called .myvcs (which should be created if it doesn't already exist).  At this point it's ok to overwrite .myvcs if it """
@@ -13,7 +14,14 @@ def backup():
 	os.system('cp -r * .myvcs/' + str(subfolder_idx))
 
 if __name__ == '__main__':
-	backup()
-
-
-
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--checkout', help = 'reverts back to selected snapshot')
+	arg = parser.parse_args()
+	if arg.checkout:
+		# check if selected snapshot is valid
+		if not os.path.isdir('.myvcs/' + org.checkout):
+			print 'requested snapshot does not exist'
+			os.exit(0)
+		os.system('cp * .myvcs/' + arg.checkout + '/* ./') 
+	else:
+		backup()
